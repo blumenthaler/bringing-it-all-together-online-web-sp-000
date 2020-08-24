38c43491ae4e8cc6049c9e22d552d84af25ca038
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-24 14:58:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog
-  attr_accessor :name, :breed, :id
   
-  def initialize(name:, breed:, id:)
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(id=nil, name, breed)
+    @id = id
     @name = name
     @breed = breed
-    @id = id
   end
-  
   
 end
