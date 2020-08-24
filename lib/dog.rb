43be857e8a797f6c6897f36bbc4e0579d600f8f9
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-08-24 15:12:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,8 +36,10 @@ class Dog
     Dog.new(id: id, name: name, breed: breed)
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE name = ?
+    SQL
     
     
   end
